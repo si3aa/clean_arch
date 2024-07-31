16d49caa46f32437c05d08a55272f15e6d081adc
si3aa <mahmoud22said22@gmail.com>
2024-07-31 15:47:31 +0300

    create weather entity

---
+++ lib/core/util/constant.dart
@@ -0,0 +1,4 @@
+class AppConstants{
+  static const String baseUrl = "https://api.openweathermap.org/data/2.5";
+  static const String apiKey = "58e47d015e88853558502fd0b289e20b";
+}
---
+++ lib/weather/domain/Entities/wether.dart
@@ -0,0 +1,15 @@
+class Wether {
+  final int id;
+  final String cityName;
+  final String main;
+  final String description;
+  final int pressure;
+
+  Wether(
+    this.id,
+    this.cityName,
+    this.main,
+    this.description,
+    this.pressure,
+  );
+}
